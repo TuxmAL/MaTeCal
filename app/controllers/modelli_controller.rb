117cf6a68bfae215f4ba3d614306697e35116f86
Antonio Liccardo <tuxmal@tiscali.it>
2013-04-15 21:01:08 +0200

    Modifiche ai Modelli.

--- app/controllers/modelli_controller.rb
+++ app/controllers/modelli_controller.rb
@@ -44,7 +44,7 @@ class ModelliController < ApplicationController
 
     respond_to do |format|
       if @modello.save
-        format.html { redirect_to @modello, notice: 'Modello was successfully created.' }
+        format.html { redirect_to @modello, notice: 'Modello creato con successo.' }
         format.json { render json: @modello, status: :created, location: @modello }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class ModelliController < ApplicationController
 
     respond_to do |format|
       if @modello.update_attributes(params[:modello])
-        format.html { redirect_to @modello, notice: 'Modello was successfully updated.' }
+        format.html { redirect_to @modello, notice: 'Modello aggiornato con successo.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
--- app/models/modello.rb
+++ app/models/modello.rb
@@ -1,3 +1,6 @@
 class Modello < ActiveRecord::Base
   attr_accessible :descrizione
+
+  validates_presence_of :descrizione
+  validates_uniqueness_of :descrizione
 end
