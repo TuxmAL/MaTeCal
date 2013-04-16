3a88b52be6fb882ee4d0cb5e1569e9f461386eeb
Antonio Liccardo <tuxmal@tiscali.it>
2013-04-16 19:31:15 +0200

    Sostituita la valdazione della presenza con una più fine validazione della lunghezza della stringa (che non può essere vuota o nil).

--- app/models/modello.rb
+++ app/models/modello.rb
@@ -2,6 +2,6 @@
 class Modello < ActiveRecord::Base
   attr_accessible :descrizione
 
-  validates_presence_of :descrizione
+  validates_length_of :descrizione, :maximum => 35, :minimum => 1, :allow_blank => false, :allow_nil => false
   validates_uniqueness_of :descrizione
 end
--- app/models/unita.rb
+++ app/models/unita.rb
@@ -2,7 +2,7 @@
 class Unita < ActiveRecord::Base
   attr_accessible :unita
 
-  validates_presence_of :unita
+  validates_length_of :unita, :maximum => 5, :minimum => 1, :allow_blank => false, :allow_nil => false
   validates_uniqueness_of :unita
 
 end
