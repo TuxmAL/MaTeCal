7fba0473b49bd4fa79baafe032ab6924671e390d
Antonio Liccardo <tuxmal@tiscali.it>
2013-04-18 00:02:38 +0200

    Modificati i validatori per non permettere valori negativi.

--- app/models/mod_pag.rb
+++ app/models/mod_pag.rb
@@ -6,8 +6,8 @@ class ModPag < ActiveRecord::Base
   validates_uniqueness_of :descrizione
   validates_inclusion_of :scadenza, :in => %w( I F ), :message => "Scadenza %{value} non valida."
   validates_inclusion_of :tipo_pagamento, :in => %w( D B ), :message => "Tipo pagamento %{value} non valido."
-  validates_numericality_of :dilazione_iniziale, :only_integer => true
-  validates_numericality_of :num_rate, :only_integer => true
+  validates_numericality_of :dilazione_iniziale, :only_integer => true,:greater_than_or_equal_to => 0
+  validates_numericality_of :num_rate, :only_integer => true, :greater_than_or_equal_to => 0
   validates_numericality_of :sconto, :only_integer => true, :greater_than_or_equal_to => 0, :less_than => 100
   validates_numericality_of :piu_sconto, :only_integer => true, :greater_than_or_equal_to => 0, :less_than => 100
 
