f6c667481012185fa721118fe3698afc24374a8b
Antonio Liccardo <tuxmal@tiscali.it>
2013-05-05 20:08:29 +0200

    Aggiunto il campo email ed inseriti una serie di controlli sui campi. Mancano i controlli  relativi a piva/codice fiscale e correttezza dell'email.

--- app/models/agente.rb
+++ app/models/agente.rb
@@ -1,7 +1,22 @@
 class Agente < ActiveRecord::Base
   belongs_to :zona
-  attr_accessible :cap, :citta, :codfis_piva, :cognome, :fax, :nome, :provincia, :provvigione, :telefono1, :telefono2,
+
+  attr_accessible :cap, :citta, :codfis_piva, :cognome, :email, :fax, :nome, :provincia, :provvigione, :telefono1, :telefono2,
                   :telefono3, :via, :zona_id
 
-  #todo inserire tutti i controlli sui campi relativi agli agenti.
+  validates_length_of :cognome, :maximum => 30, :minimum => 1, :allow_blank => false, :allow_nil => false  validates_length_of :cognome, :maximum => 30, :minimum => 1, :allow_blank => false, :allow_nil => false
+  validates_length_of :nome, :maximum => 25, :minimum => 1, :allow_blank => false, :allow_nil => false
+  validates_length_of :via, :maximum => 40, :minimum => 1, :allow_blank => false, :allow_nil => false
+  validates_length_of :citta, :maximum => 50, :minimum => 1, :allow_blank => false, :allow_nil => false
+  validates_length_of :provincia, :is => 2, :allow_blank => false, :allow_nil => false
+  validates_length_of :cap, :is => 5, :allow_blank => false, :allow_nil => false
+  validates_length_of :telefono1, :maximum => 15, :minimum => 3, :allow_blank => true, :allow_nil => true
+  validates_length_of :telefono2, :maximum => 15, :minimum => 3, :allow_blank => true, :allow_nil => true
+  validates_length_of :telefono3, :maximum => 15, :minimum => 3, :allow_blank => true, :allow_nil => true
+  validates_length_of :fax, :maximum => 30, :minimum => 3, :allow_blank => true, :allow_nil => true
+  validates_length_of :email,:maximum => 30, :minimum => 6, :allow_blank => true, :allow_nil => true
+  validates_length_of :codfis_piva,:maximum => 15, :minimum => 11, :allow_blank => false, :allow_nil => false
+  validates_presence_of :zona_id
+
+  #todo inserire i controlli sull'email e sul checksum della partita iva / codice fiscale.
 end
