de278e6b2299e34babef37a558d7dc879fda7a69
Antonio Liccardo <tuxmal@tiscali.it>
2013-05-08 01:18:09 +0200

    Aggiunto validatore custom per le email.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,3 +1,40 @@
-class EmailValidator
-  # To change this template use File | Settings | File Templates.
+require 'mail'
+# vedi http://my.rails-royce.org/2010/07/21/email-validation-in-ruby-on-rails-without-regexp/
+# ed in particolare: https://gist.github.com/1188367
+#
+class EmailValidator < ActiveModel::EachValidator
+  attr_reader :record, :attribute, :value, :email, :tree
+
+  def validate_each(record, attribute, value)
+    @record, @attribute, @value = record, attribute, value
+
+    @email = Mail::Address.new(value)
+    @tree = email.__send__(:tree)
+
+    add_error unless valid?
+  rescue Mail::Field::ParseError
+    add_error
+  end
+
+  private
+
+  def valid?
+    !!(domain_and_address_present? && domain_has_more_than_one_atom?)
+  end
+
+  def domain_and_address_present?
+    email.domain && email.address == value
+  end
+
+  def domain_has_more_than_one_atom?
+    tree.domain.dot_atom_text.elements.length > 1
+  end
+
+  def add_error
+    if message = options[:message]
+      record.errors[attribute] << message
+    else
+      record.errors.add(attribute, :invalid)
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module MaTeCal
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/app/validators)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -36,7 +37,7 @@ module MaTeCal
     config.i18n.default_locale = :it
     config.i18n.available_locales = :it
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding = 'utf-8'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
